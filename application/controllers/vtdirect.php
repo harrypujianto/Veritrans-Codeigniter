58ec42de5c290625213e1c6fd6505513aba8aaed
Harry <harry.pujianto@veritrans.co.id>
2016-08-04 11:15:05 +0700

    Remove sample server key

--- application/controllers/vtdirect.php
+++ application/controllers/vtdirect.php
@@ -22,7 +22,7 @@ class Vtdirect extends CI_Controller {
 	public function __construct()
     {
         parent::__construct();
-        $params = array('server_key' => 'VT-server-UJ4uPuXhwiNXwhpQx5-S76U1', 'production' => false);
+        $params = array('server_key' => 'your_server_key', 'production' => false);
 		$this->load->library('veritrans');
 		$this->veritrans->config($params);
 		$this->load->helper('url');
