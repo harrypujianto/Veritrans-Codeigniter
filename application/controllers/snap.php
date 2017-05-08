9e771076f0c0c0d12fb3b4f312edd59fb5f79d54
Harry <harry.pujianto@veritrans.co.id>
2017-05-08 15:41:13 +0700

    fix & update
    - fix 3ds issue
    - add custom expiry sample code
    - change variable name

--- application/controllers/snap.php
+++ application/controllers/snap.php
@@ -93,14 +93,28 @@ class Snap extends CI_Controller {
 		  'shipping_address' => $shipping_address
 		);
 
-		// Fill transaction details
-		$transaction = array(
-		  'transaction_details' => $transaction_details,
-		  'customer_details' => $customer_details,
-		  'item_details' => $item_details,
-		);
-		//error_log(json_encode($transaction));
-		$snapToken = $this->midtrans->getSnapToken($transaction);
+		// Data yang akan dikirim untuk request redirect_url.
+        $credit_card['secure'] = true;
+        //ser save_card true to enable oneclick or 2click
+        //$credit_card['save_card'] = true;
+
+        $time = time();
+        $custom_expiry = array(
+            'start_time' => date("Y-m-d H:i:s O",$time),
+            'unit' => 'minute', 
+            'duration'  => 2
+        );
+        
+        $transaction_data = array(
+            'transaction_details'=> $transaction_details,
+            'item_details'       => $item_details,
+            'customer_details'   => $customer_details,
+            'credit_card'        => $credit_card,
+            'expiry'             => $custom_expiry
+        );
+
+		error_log(json_encode($transaction_data));
+		$snapToken = $this->midtrans->getSnapToken($transaction_data);
 		error_log($snapToken);
 		echo $snapToken;
     }
